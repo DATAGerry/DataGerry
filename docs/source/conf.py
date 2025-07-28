97fbd71ef2e2649259bcf05990e1f13fada303dd
jaysemere <jakob.semere@becon.de>
2025-07-28 20:19:52 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,6 @@ language = 'en'
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-html_extra_path = ['rest_api']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
