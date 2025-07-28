664c3f93d4e3ef169001b444fba741e1ef019bfc
jaysemere <jakob.semere@becon.de>
2025-07-28 20:30:52 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,7 +105,7 @@ html_favicon = '_static/img/datagerry_favicon.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_extra_path = ['rest_api']
+html_extra_path = ['swaggerui']
 
 html_css_files = [
     'css/custom.css',
