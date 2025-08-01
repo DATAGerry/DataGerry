81fcd02cd84b2fded24102673d705cd8b60c3ec1
Abdullah Khalid <Abdullah.Khalid@becon.de>
2025-08-01 10:36:36 +0200

    Update environment.cloud.ts

--- app/src/environments/environment.cloud.ts
+++ app/src/environments/environment.cloud.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     cloudMode: true,
     preCloudMode: true,
-    featurePreviewMode: true,
+    featurePreviewMode: false,
     protocol: 'http',
-    apiUrl: '127.0.1.1', // API URL
-    apiPort: 4000 // Typically HTTPS
+    apiUrl: '192.168.64.2', // API URL
+    apiPort: 4000 
 };
